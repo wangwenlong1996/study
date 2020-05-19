0afd7afaa3472769aef0a65c239a45ba4d5a3adb
wangwenlong1996 <38375783+wangwenlong1996@users.noreply.github.com>
2020-05-19 21:23:16 +0800

    Merge branch 'master' of github.com:wangwenlong1996/study into myself_computer
    单例模式与策略模式

---
+++ java/02设计模式/02self-code/tank-demo/src/main/java/name/wwl/demo/study/design/pattern/strategy/CatWightComparator.java
@@ -0,0 +1,10 @@
+package name.wwl.demo.study.design.pattern.strategy;
+
+public class CatWightComparator implements Comparator<Cat> {
+    @Override
+    public int compare(Cat o1, Cat o2) {
+        if (o1.weight>o2.weight) return -1;
+        if (o1.weight<o2.weight) return 1;
+        return 0;
+    }
+}
---
+++ java/02设计模式/02self-code/tank-demo/src/main/java/name/wwl/demo/study/design/pattern/strategy/Comparator.java
@@ -0,0 +1,5 @@
+package name.wwl.demo.study.design.pattern.strategy;
+
+public interface Comparator<T> {
+    int compare(T o1, T o2);
+}
