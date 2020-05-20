bb0a34033356f4885a1b310f7373a1d4a61bbb52
wangwenlong1996 <38375783+wangwenlong1996@users.noreply.github.com>
2020-05-20 17:32:42 +0800

    简单工厂+抽象工厂

---
+++ java/02设计模式/02self-code/tank-demo/src/main/java/name/wwl/demo/study/design/pattern/factory/abstractfactory/Food.java
@@ -0,0 +1,17 @@
+package name.wwl.demo.study.design.pattern.factory.abstractfactory;
+
+/**
+ * @Description:
+ * @Author: 王文龙
+ * @Date: 2020/5/20 17:12
+ */
+public abstract class Food {
+    /**
+     *  打印食品名字
+     * @Author 王文龙
+     * @Date 17:13 2020/5/20
+     *
+     * @return void
+     */
+    abstract void printName();
+}
---
+++ java/02设计模式/02self-code/tank-demo/src/main/java/name/wwl/demo/study/design/pattern/factory/abstractfactory/Weapon.java
@@ -0,0 +1,10 @@
+package name.wwl.demo.study.design.pattern.factory.abstractfactory;
+
+/**
+ * @Description:
+ * @Author: 王文龙
+ * @Date: 2020/5/20 17:14
+ */
+public abstract class Weapon {
+    abstract void shoot();
+}
